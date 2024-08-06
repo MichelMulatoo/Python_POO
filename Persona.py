35c807a4dd4e8a826d21d17d4a8b815b9b7a89d3
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-06 13:06:34 -0400

    Persona actualizadas

--- Persona.py
+++ Persona.py
@@ -1,20 +1,42 @@
 #En esta clase se crean 2 objetos personas
 class Persona:
-    def __init__(self, nombre, apellido, edad, *valores, **terminos):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.valores = valores
-        self.terminos = terminos
+    def __init__(self, nombre, apellido, edad):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+
+    @property
+    def nombre(self):
+        print('llamando metodo get nombre')
+        return self._nombre
+
+    @nombre.setter#modifica el valor de atributo
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def edad(self):
+        return self._edad
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
 
     def mostrar_detalles(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}')
+        print(f'Persona: {self._nombre} {self._apellido} {self._edad}')
 
 
-persona1 = Persona('Michel', 'Mulato','31', 2, 3, 5, m='manzana', p='pera')
+persona1 = Persona('Michel', 'Mulato','31')
+persona1.nombre = 'Dayana Carrasco'
+
+persona1.apellido = 'Carrasco'
+persona1.edad = 25
 persona1.mostrar_detalles()
-
-
-
-persona2 = Persona('Dayana', 'Droguet', '32')
-persona2.mostrar_detalles()
