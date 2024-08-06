9b4469f72afdde53e86c19e6a963b53812d52bbc
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-06 13:22:48 -0400

    Prueba persona inicio

---
+++ PruebaPersona.py
@@ -0,0 +1,14 @@
+from Persona import  Persona
+
+
+print('Creacion objetos'.center(30, '-'))
+persona1 = Persona('Samy', 'Carrasco,', 7)
+persona1.mostrar_detalles()
+
+
+
+print('Eliminacion objetos'.center(30, '-'))
+del persona1
+
+
+
