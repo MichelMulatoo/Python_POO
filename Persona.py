8bfaf1b9aa02b19f5416779fd32ecf2654ecc549
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-06 12:23:12 -0400

    Persona actualizada

--- Persona.py
+++ Persona.py
@@ -1,17 +1,19 @@
 #En esta clase se crean 2 objetos personas
 class Persona:
-    def __init__(self, nombre, apellido, edad):
+    def __init__(self, nombre, apellido, edad, *valores, **terminos):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+        self.valores = valores
+        self.terminos = terminos
 
     def mostrar_detalles(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}')
 
 
-persona1 = Persona('Michel', 'Mulato','31')
+persona1 = Persona('Michel', 'Mulato','31', 2, 3, 5, m='manzana', p='pera')
 persona1.mostrar_detalles()
-#Persona.mostrar_detalles(persona1)
+
 
 
 persona2 = Persona('Dayana', 'Droguet', '32')
