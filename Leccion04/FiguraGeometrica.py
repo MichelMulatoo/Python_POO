ac2bea0f660a1b22c31490511bae5136e32a9d86
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-08 18:17:39 -0400

    Figura Geometrica Actualizada

--- Leccion04/FiguraGeometrica.py
+++ Leccion04/FiguraGeometrica.py
@@ -1,7 +1,17 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            self._alto = 0
+            print(f'Valor incorrecto, ancho: {ancho}')
+
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            self._ancho = 0
+            print(f'Valor incorrecto, alto: {alto}')
+
 
     @property
     def ancho(self):
@@ -9,7 +19,11 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor incorrecto, ancho: {ancho}')
+
 
     @property
     def alto(self):
@@ -17,7 +31,13 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            print(f'Valor incorrecto, alto: {alto}')
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho:{self._ancho}, Alto: {self._alto}]'
+
+    def _validar_valor(self, valor):
+        return True if 0 < valor < 10 else False
