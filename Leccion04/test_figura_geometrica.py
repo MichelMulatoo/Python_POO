4ecff83d4b5d59dce5bd26acf656ad48857caefc
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-08 18:17:15 -0400

    Tes de Figura Geometrica Actualizada

--- Leccion04/test_figura_geometrica.py
+++ Leccion04/test_figura_geometrica.py
@@ -4,7 +4,8 @@ from Rectangulo import Rectangulo
 
 print('Creacion objeto cuadrado'.center(50, '-') )
 cuadrado1 = Cuadrado(5,'rojo')
-
+cuadrado1.alto = 9
+cuadrado1.ancho = 9
 print(f'Calculo Area cuadrado:{cuadrado1.calcular_area()}')
 
 #MRO- Method Resolution Order
@@ -12,5 +13,7 @@ print(f'Calculo Area cuadrado:{cuadrado1.calcular_area()}')
 print(cuadrado1)
 print('Creacion objeto rectangulo'.center(50, '-') )
 rectangulo1 = Rectangulo(3, 8 ,'Verde')
+rectangulo1.ancho = 15
+
 print(f'Calculo area Rectangulo: {rectangulo1.calcularArea()}')
 print(rectangulo1)
