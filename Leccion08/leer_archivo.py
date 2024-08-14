2a19aeb955fa5c2f0bd0742ad930f415b00fb7c4
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-08-13 22:11:45 -0400

    Leer actualizada

---
+++ Leccion08/leer_archivo.py
@@ -0,0 +1,12 @@
+
+
+#archivo = open('C:\\PythonPOO\\POO\\Leccion08\\prueba.txt', 'r', encoding='utf8')
+archivo = open('prueba.txt', 'r', encoding='utf8')
+
+
+#Leer algunos caracteres
+#print(archivo.read())
+# print(archivo.read(21))
+
+#Leer lineas completas
+print(archivo.readline())
