0b2c7451683128d30fa55bd300c3d327d6bd4dd5
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-10-16 13:03:38 -0300

    update de BD

--- BD/Leccion01/prueba_bd.py
+++ BD/Leccion01/prueba_bd.py
@@ -29,6 +29,19 @@ import psycopg2
 conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db'
 )
 
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input('Ingresa el id_persona a eliminar (Separados por coma): ')
+            valores = (tuple(entrada.split(',')),)
+            cursor. execute(sentencia, valores)
+            #conexion.commit()
+            regitros_eliminados = cursor.rowcount
+            print(f'Registro eliminado: ¨{regitros_eliminados}')
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
 # try:
 #     with conexion:
 #         with conexion.cursor() as cursor:
@@ -40,21 +53,24 @@ conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',po
 #             for registro in registros:
 #                 print(registros)
 
-
-try:
-    with conexion:
-        with conexion.cursor() as cursor:
-            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-            valores =('Juan Carlos', 'Suarez', 'jcjuarez@gmail.com', 1)
-            cursor. execute(sentencia, valores)
-            #conexion.commit()
-            registros_Actualizados = cursor.rowcount
-            print(f'Registros Actualizado: ¨{registros_Actualizados}')
-except Exception as e:
-    print(f'Ocurrio un error: {e}')
+#Metodo para actualizar varios registros
+# try:
+#     with conexion:
+#         with conexion.cursor() as cursor:
+#             sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+#             valores =(
+#                       ('Juan', 'Perez', 'jcperez@gmail.com', 1),
+#                      ('Ivonnean ', 'Gutierrez', 'igutierrez@gmail.com', 2)
+#             )
+#             cursor. executemany(sentencia, valores)
+#             #conexion.commit()
+#             registros_Actualizados = cursor.rowcount
+#             print(f'Registros Actualizado: ¨{registros_Actualizados}')
+# except Exception as e:
+#     print(f'Ocurrio un error: {e}')
     
-finally:
-    conexion.close
+# finally:
+#     conexion.close
 # try:
 #     with conexion:
 #         with conexion.cursor() as cursor:
